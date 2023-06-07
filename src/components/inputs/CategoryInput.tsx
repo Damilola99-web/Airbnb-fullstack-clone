f22fd984ea992170c6c05c8d64c7dd4017cbf1d2
Damilola99-web <rasheedw622@gmail.com>
2023-06-07 14:21:09 +0100

    .

--- src/components/inputs/CategoryInput.tsx
+++ src/components/inputs/CategoryInput.tsx
@@ -16,7 +16,7 @@ export default function CategoryInput({
 		<div
 			onClick={() => onClick(label)}
 			className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${
-				selected ? 'border-black' : 'bg-neutral-200'
+				selected ? 'border-black' : 'border-neutral-200'
 			}`}
 		>
 			<Icon size={30} />
--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -86,7 +86,7 @@ export default function RentModal() {
 				title='Which of these best describe your place?'
 				subtitle='Pick a category'
 			/>
-			<div className=' grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
+			<div className=' grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto w-full'>
 				{categories.map((item) => (
 					<div
 						key={item.label}
